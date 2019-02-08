6431b8076da846695450f32cf1562e88412aa96d
Dave Wadden <dwadden@cs.washington.edu>
2019-02-07 23:59:07 -0800

    Reproducing coref results from scierc paper.
    No ELMO

--- data/__init__.py
+++ data/__init__.py
@@ -1,2 +1,3 @@
 from dygie.data.dataset_readers.ie_json import IEJsonReader
+from dygie.data.dataset_readers.coref_json import CorefJsonReader
 from dygie.data.iterators.document_iterator import DocumentIterator
