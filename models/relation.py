bb584ac81d814e04e5ca4970c37ec9153758f087
Dave Wadden <dwadden@cs.washington.edu>
2019-02-19 18:18:06 -0800

    Get rid of debugging tests of accidental transpositions.

--- models/relation.py
+++ models/relation.py
@@ -182,7 +182,6 @@ class RelationExtractor(Model):
 
         similarity_embeddings = embeddings_1_expanded * embeddings_2_expanded
 
-        # In case the order got flipped somehow
         pair_embeddings_list = [embeddings_1_tiled, embeddings_2_tiled, similarity_embeddings]
         pair_embeddings = torch.cat(pair_embeddings_list, dim=3)
 
@@ -200,7 +199,6 @@ class RelationExtractor(Model):
         dummy_scores = relation_scores.new_zeros(*shape)
 
         relation_scores = torch.cat([dummy_scores, relation_scores], -1)
-        relation_scores = relation_scores.transpose(1, 2)
         return relation_scores
 
     @staticmethod
