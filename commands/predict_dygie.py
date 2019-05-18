8023f11c2a197731c25809dda638b2e3a38ee5ae
Dave Wadden <dwadden@cs.washington.edu>
2019-05-17 17:45:35 -0700

    Output predictions.

--- commands/predict_dygie.py
+++ commands/predict_dygie.py
@@ -16,30 +16,27 @@ from sys import argv
 import numpy as np
 
 from allennlp.models.archival import load_archive
-from allennlp.predictors.predictor import Predictor
 from allennlp.common.util import import_submodules
 from allennlp.data import DatasetReader
 from allennlp.data.dataset import Batch
+from allennlp.nn import util as nn_util
 
 from dygie.data.iterators.document_iterator import DocumentIterator
 
 
 decode_fields = dict(coref="clusters",
                      ner="decoded_ner",
-                     relation="decoded_relations",
-                     events="decoded_events")
+                     relation="decoded_relations")
 
-decode_names = dict(coref="clusters",
-                    ner="ner",
-                    relation="relations",
-                    events="events")
+decode_names = dict(coref="predicted_clusters",
+                    ner="predicted_ner",
+                    relation="predicted_relations")
 
 
 def cleanup(k, decoded, sentence_starts):
     dispatch = {"coref": cleanup_coref,
                 "ner": cleanup_ner,
-                "relation": cleanup_relation,
-                "events": lambda x, y: x}  # TODO(dwadden) make this nicer later if worth it.
+                "relation": cleanup_relation}
     return dispatch[k](decoded, sentence_starts)
 
 
@@ -80,9 +77,50 @@ def cleanup_relation(decoded, sentence_starts):
     return res
 
 
-def predict(archive_file, test_file, output_file):
+def cleanup_event(decoded, sentence_starts):
+    assert len(decoded) == len(sentence_starts)  # Length check.
+    res = []
+    for sentence, sentence_start in zip(decoded, sentence_starts):
+        trigger_dict = sentence["trigger_dict"]
+        argument_dict = sentence["argument_dict_with_scores"]
+        this_sentence = []
+        for trigger_ix, trigger_label in trigger_dict.items():
+            this_event = []
+            this_event.append([trigger_ix + sentence_start, trigger_label])
+            event_arguments = {k: v for k, v in argument_dict.items() if k[0] == trigger_ix}
+            this_event_args = []
+            for k, v in event_arguments.items():
+                entry = [x + sentence_start for x in k[1]] + list(v)
+                this_event_args.append(entry)
+            this_event_args = sorted(this_event_args, key=lambda entry: entry[0])
+            this_event.extend(this_event_args)
+            this_sentence.append(this_event)
+        res.append(this_sentence)
+
+    return res
+
+
+def load_json(test_file):
+    res = []
+    with open(test_file, "r") as f:
+        for line in f:
+            res.append(json.loads(line))
+
+    return res
+
+
+def check_lengths(d):
+    "Make sure all entries in dict have same length."
+    keys = list(d.keys())
+    keys.remove("doc_key")
+    lengths = [len(d[k]) for k in keys]
+    assert len(set(lengths)) == 1
+
+
+def predict(archive_file, test_file, output_file, cuda_device):
     import_submodules("dygie")
-    archive = load_archive(archive_file)
+    gold_test_data = load_json(test_file)
+    archive = load_archive(archive_file, cuda_device)
     model = archive.model
     model.eval()
     config = archive.config.duplicate()
@@ -93,18 +131,22 @@ def predict(archive_file, test_file, output_file):
     batch.index_instances(model.vocab)
     iterator = DocumentIterator()
     with open(output_file, "w") as f:
-        for doc in iterator(batch.instances, num_epochs=1):
+        for doc, gold_data in zip(iterator(batch.instances, num_epochs=1, shuffle=False),
+                                  gold_test_data):
+            doc = nn_util.move_to_device(doc, cuda_device)  # Put on GPU.
             sentence_lengths = [len(entry["sentence"]) for entry in doc["metadata"]]
             sentence_starts = np.cumsum(sentence_lengths)
             sentence_starts = np.roll(sentence_starts, 1)
             sentence_starts[0] = 0
             pred = model(**doc)
             decoded = model.decode(pred)
-            res = {}
+            predictions = {}
             for k, v in decoded.items():
-                res[decode_names[k]] = cleanup(k, v[decode_fields[k]], sentence_starts)
-            res["sentences"] = [entry["sentence"] for entry in doc["metadata"]]
-            res["doc_key"] = doc["metadata"][0]["doc_key"]
+                predictions[decode_names[k]] = cleanup(k, v[decode_fields[k]], sentence_starts)
+            res = {}
+            res.update(gold_data)
+            res.update(predictions)
+            check_lengths(res)
             encoded = json.dumps(res, default=int)
             f.write(encoded + "\n")
 
@@ -113,7 +155,8 @@ def main():
     archive_file = argv[1]
     test_file = argv[2]
     output_file = argv[3]
-    predict(archive_file, test_file, output_file)
+    cuda_device = int(argv[4])
+    predict(archive_file, test_file, output_file, cuda_device)
 
 
 if __name__ == '__main__':
