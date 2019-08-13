9e1afad2c57ea4f9f179af13829139ca8c5b8414
Dave Wadden <dwadden@cs.washington.edu>
2019-08-12 22:31:37 -0700

    Switch from 2 to 4 space indent.

--- scripts/data/genia/check_genia_xml_sutd.py
+++ scripts/data/genia/check_genia_xml_sutd.py
@@ -11,59 +11,59 @@ import random
 
 
 def check_against_xml_source():
-  "Make sure my processed documents are consistent with the source XML"
-  processed_dir = "/data/dave/proj/scierc_coref/data/genia/sutd-article/final"
-  xml_file = "/data/dave/proj/scierc_coref/data/genia/GENIAcorpus3.02p/GENIAcorpus3.02.merged.xml"
+    "Make sure my processed documents are consistent with the source XML"
+    processed_dir = "/data/dave/proj/scierc_coref/data/genia/sutd-article/final"
+    xml_file = "/data/dave/proj/scierc_coref/data/genia/GENIAcorpus3.02p/GENIAcorpus3.02.merged.xml"
 
-  with open(xml_file, 'r') as infile:
-    soup = BS(infile.read().decode('utf-8'), 'lxml')
-  articles = soup.find_all('article')
+    with open(xml_file, 'r') as infile:
+        soup = BS(infile.read().decode('utf-8'), 'lxml')
+    articles = soup.find_all('article')
 
-  # Check 5 random files
+    # Check 5 random files
 
-  files = os.listdir(processed_dir)
-  random.shuffle(files)
-  files = files[:5]
+    files = os.listdir(processed_dir)
+    random.shuffle(files)
+    files = files[:5]
 
-  for processed_file in files:
-    with open(path.join(processed_dir, processed_file), 'r') as f:
-      lines = f.readlines()
-      art_text = "".join(x for x in lines[::4]).replace(" ,", "")
+    for processed_file in files:
+        with open(path.join(processed_dir, processed_file), 'r') as f:
+            lines = f.readlines()
+            art_text = "".join(x for x in lines[::4]).replace(" ,", "")
 
-    found_one = False
-    for article in articles:
-      this_text = article.get_text()
-      if art_text[:50] in this_text or art_text[200:250] in this_text:
-        found_one = True
-        print("Got one.")
-        print(this_text)
-        print()
-        print(art_text)
-        print()
-    if not found_one:
-      print("Didn't find one.")
-      print(art_text)
-    print(40 * "#")
+        found_one = False
+        for article in articles:
+            this_text = article.get_text()
+            if art_text[:50] in this_text or art_text[200:250] in this_text:
+                found_one = True
+                print("Got one.")
+                print(this_text)
+                print()
+                print(art_text)
+                print()
+        if not found_one:
+            print("Didn't find one.")
+            print(art_text)
+        print(40 * "#")
 
 
 def check_against_sutd_processed():
-  """
-  Grab one of my processed documents. Make sure I can find a match in one of
-  the SUTD processed files.
-  """
-  theirs_file = "/data/dave/proj/scierc_coref/data/genia/sutd/test.data"
-  with open(theirs_file, "r") as f:
-    theirs = f.readlines()
+    """
+    Grab one of my processed documents. Make sure I can find a match in one of
+    the SUTD processed files.
+    """
+    theirs_file = "/data/dave/proj/scierc_coref/data/genia/sutd/test.data"
+    with open(theirs_file, "r") as f:
+        theirs = f.readlines()
 
-  mine_file = "/data/dave/proj/scierc_coref/data/genia/sutd-article/final/90244434.tok.5types.no_disc.data"
-  with open(mine_file, "r") as f:
-    mine = f.readlines()
+    mine_file = "/data/dave/proj/scierc_coref/data/genia/sutd-article/final/90244434.tok.5types.no_disc.data"
+    with open(mine_file, "r") as f:
+        mine = f.readlines()
 
-  my_first = mine[0]
-  foo = [entry == my_first for entry in theirs]
-  ix = [i for i, entry in enumerate(foo) if entry][0]
-  theirs_match = theirs[ix:ix + len(mine)]
+    my_first = mine[0]
+    foo = [entry == my_first for entry in theirs]
+    ix = [i for i, entry in enumerate(foo) if entry][0]
+    theirs_match = theirs[ix:ix + len(mine)]
 
-  same = [x == y for x, y in zip(mine, theirs_match)]
-  print(sum(same))
-  print(len(mine))
+    same = [x == y for x, y in zip(mine, theirs_match)]
+    print(sum(same))
+    print(len(mine))
--- scripts/data/genia/genia_align_ids.py
+++ scripts/data/genia/genia_align_ids.py
@@ -14,95 +14,95 @@ import numpy as np
 from Levenshtein.StringMatcher import StringMatcher
 
 def make_lookups():
-  """
-  Need to match the ner data and the coref data by sentence. Create
-  dictionaries where the keys are the document ID's and the values are the
-  sentences. Save as .pkl files to match.
-  """
-  xml_path = "/data/dave/proj/scierc_coref/data/genia/GENIAcorpus3.02p/GENIAcorpus3.02.merged.xml"
-  coref_path = "/data/dave/proj/scierc_coref/data/genia/GENIA_MedCo_coreference_corpus_1.0"
+    """
+    Need to match the ner data and the coref data by sentence. Create
+    dictionaries where the keys are the document ID's and the values are the
+    sentences. Save as .pkl files to match.
+    """
+    xml_path = "/data/dave/proj/scierc_coref/data/genia/GENIAcorpus3.02p/GENIAcorpus3.02.merged.xml"
+    coref_path = "/data/dave/proj/scierc_coref/data/genia/GENIA_MedCo_coreference_corpus_1.0"
 
-  with open(xml_path, 'r') as infile:
-    soup = BS(infile.read().decode('utf-8'), 'lxml')
+    with open(xml_path, 'r') as infile:
+        soup = BS(infile.read().decode('utf-8'), 'lxml')
 
-  articles = soup.find_all('article')
-  articles_obj = []
+    articles = soup.find_all('article')
+    articles_obj = []
 
-  def get_ner_info(article):
-    medline_id = article.find("bibliomisc").text
-    title = article.find("title").text.strip()
-    return (medline_id, title)
+    def get_ner_info(article):
+        medline_id = article.find("bibliomisc").text
+        title = article.find("title").text.strip()
+        return (medline_id, title)
 
-  ner_info = dict([get_ner_info(entry) for entry in articles])
+    ner_info = dict([get_ner_info(entry) for entry in articles])
 
-  with open("/data/dave/proj/scierc_coref/data/genia/align/ner_ids.pkl", "wb") as f:
-    pkl.dump(ner_info, f, protocol=-1)
+    with open("/data/dave/proj/scierc_coref/data/genia/align/ner_ids.pkl", "wb") as f:
+        pkl.dump(ner_info, f, protocol=-1)
 
-  coref_files = glob.glob(path.join(coref_path, "*.xml"))
+    coref_files = glob.glob(path.join(coref_path, "*.xml"))
 
-  def get_coref_info(name):
-    with open(name, "r") as f:
-      this_soup = BS(f.read().decode('utf-8'), 'lxml')
-      pmid = this_soup.find("pmid").text
-      title = this_soup.find("articletitle").text.strip()
-      return (pmid, title)
+    def get_coref_info(name):
+        with open(name, "r") as f:
+            this_soup = BS(f.read().decode('utf-8'), 'lxml')
+            pmid = this_soup.find("pmid").text
+            title = this_soup.find("articletitle").text.strip()
+            return (pmid, title)
 
-  coref_info = dict([get_coref_info(entry) for entry in coref_files])
+    coref_info = dict([get_coref_info(entry) for entry in coref_files])
 
-  with open("/data/dave/proj/scierc_coref/data/genia/align/coref_ids.pkl", "wb") as f:
-    pkl.dump(coref_info, f, protocol=-1)
+    with open("/data/dave/proj/scierc_coref/data/genia/align/coref_ids.pkl", "wb") as f:
+        pkl.dump(coref_info, f, protocol=-1)
 
 
 def create_matches():
-  """
-  Create a dataset mapping the ner id's to the coref id's and vice versa.
-  """
-  def make_comparator():
-    matcher = StringMatcher()
+    """
+    Create a dataset mapping the ner id's to the coref id's and vice versa.
+    """
+    def make_comparator():
+        matcher = StringMatcher()
 
-    def compare(str1, str2):
-      matcher.set_seqs(str1, str2)
-      return matcher.distance()
-    return compare
+        def compare(str1, str2):
+            matcher.set_seqs(str1, str2)
+            return matcher.distance()
+        return compare
 
 
-  with open("/data/dave/proj/scierc_coref/data/genia/align/coref_ids.pkl", "rb") as f:
-    coref = pkl.load(f).items()
-  with open("/data/dave/proj/scierc_coref/data/genia/align/ner_ids.pkl", "rb") as f:
-    ner = pkl.load(f).items()
+    with open("/data/dave/proj/scierc_coref/data/genia/align/coref_ids.pkl", "rb") as f:
+        coref = pkl.load(f).items()
+    with open("/data/dave/proj/scierc_coref/data/genia/align/ner_ids.pkl", "rb") as f:
+        ner = pkl.load(f).items()
 
-  compare = make_comparator()
+    compare = make_comparator()
 
-  matched = set()
-  matches = []
-  # Loop over all the coref entries. Find the ner entry that's closest in edit
-  # distance. If it's not 0 in edit distance, print it. Nonzero can happen
-  # because of minor differences in editing.
-  for article in coref:
-    sentence = article[1]
-    distance = [compare(sentence, entry[1]) for entry in ner]
-    the_match = np.argmin(distance)
-    match_article = ner[the_match]
-    match_distance = distance[the_match]
-    # Make sure we're not double-matching.
-    if the_match in matched:
-      raise Exception("Already matched.")
-    if match_distance:
-      print sentence
-      print match_article[1]
-    matches.append(dict(ner=match_article[0].replace("MEDLINE:", ""),
-                        coref=article[0]))
-    matched.add(the_match)
+    matched = set()
+    matches = []
+    # Loop over all the coref entries. Find the ner entry that's closest in edit
+    # distance. If it's not 0 in edit distance, print it. Nonzero can happen
+    # because of minor differences in editing.
+    for article in coref:
+        sentence = article[1]
+        distance = [compare(sentence, entry[1]) for entry in ner]
+        the_match = np.argmin(distance)
+        match_article = ner[the_match]
+        match_distance = distance[the_match]
+        # Make sure we're not double-matching.
+        if the_match in matched:
+            raise Exception("Already matched.")
+        if match_distance:
+            print sentence
+            print match_article[1]
+        matches.append(dict(ner=match_article[0].replace("MEDLINE:", ""),
+                                                coref=article[0]))
+        matched.add(the_match)
 
-  matches = pd.DataFrame(matches)
-  outfile = "/data/dave/proj/scierc_coref/data/genia/align/alignment.csv"
-  matches.to_csv(outfile, index=False)
-  print "All done."
+    matches = pd.DataFrame(matches)
+    outfile = "/data/dave/proj/scierc_coref/data/genia/align/alignment.csv"
+    matches.to_csv(outfile, index=False)
+    print "All done."
 
 
 def main():
-  create_matches()
+    create_matches()
 
 
 if __name__ == '__main__':
-  main()
+    main()
