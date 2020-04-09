f8ab476389eb2d1bd8209b1d0f452161e6e2f9dd
Dave Wadden <dwadden@cs.washington.edu>
2020-04-09 00:10:53 -0700

    Script to retrieve chemprot data.

---
+++ scripts/data/get_chemprot.sh
@@ -0,0 +1,20 @@
+# Download chemprot and format for model training.
+# From https://biocreative.bioinformatics.udel.edu/news/corpora/chemprot-corpus-biocreative-vi/.
+
+out_dir=data/chemprot
+mkdir $out_dir
+
+# Download and unzip.
+mkdir $out_dir/raw_data
+wget https://biocreative.bioinformatics.udel.edu/media/store/files/2017/ChemProt_Corpus.zip -P $out_dir/raw_data
+unzip $out_dir/raw_data/ChemProt_Corpus.zip -d $out_dir/raw_data
+
+unzip_dir=$out_dir/raw_data/ChemProt_Corpus
+ls $unzip_dir | while read name
+do
+    unzip $unzip_dir/$name -d $unzip_dir
+done
+
+rm -r $unzip_dir/__MACOSX
+rm -r $unzip_dir/*.zip
+rm -r $out_dir/raw_data/ChemProt_Corpus.zip
