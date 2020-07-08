0ca9c4dcc3471543253e7c5b1dc45910b9c88b4c
Gilles J <9109832+GillesJ@users.noreply.github.com>
2020-07-08 12:48:17 +0200

    make downloader scripts Dockerfile compatible

--- scripts/data/get_chemprot.sh
+++ scripts/data/get_chemprot.sh
@@ -1,11 +1,11 @@
 # Download chemprot and format for model training.
 # From https://biocreative.bioinformatics.udel.edu/news/corpora/chemprot-corpus-biocreative-vi/.
 
-out_dir=data/chemprot
-mkdir $out_dir
+out_dir=./data/chemprot
+mkdir -p $out_dir
 
 # Download and unzip.
-mkdir $out_dir/raw_data
+mkdir -p $out_dir/raw_data
 wget https://biocreative.bioinformatics.udel.edu/media/store/files/2017/ChemProt_Corpus.zip -P $out_dir/raw_data
 unzip $out_dir/raw_data/ChemProt_Corpus.zip -d $out_dir/raw_data
 
@@ -20,12 +20,12 @@ rm -r $unzip_dir/*.zip
 rm -r $out_dir/raw_data/ChemProt_Corpus.zip
 
 # Get rid of the `_gs` suffix on the test data.
-python scripts/data/chemprot/01_rename_test.py
+python ./scripts/data/chemprot/01_rename_test.py
 
 # Run formatting.
-mkdir $out_dir/processed_data
+mkdir -p $out_dir/processed_data
 python scripts/data/chemprot/02_chemprot_to_input.py
 
 # Spot-check output
 echo "Spot-checking."
-python scripts/data/chemprot/03_spot_check.py
+python ./scripts/data/chemprot/03_spot_check.py
--- scripts/data/get_scierc.sh
+++ scripts/data/get_scierc.sh
@@ -2,8 +2,8 @@
 # folder.
 # Usage: From main project folder, run `bash scripts/data/get_scierc.sh`
 
-out_dir=data/scierc
-mkdir $out_dir
+out_dir=./data/scierc
+mkdir -p $out_dir
 
 # Download.
 wget http://nlp.cs.washington.edu/sciIE/data/sciERC_raw.tar.gz -P $out_dir
