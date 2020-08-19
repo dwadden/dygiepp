b505104f9335bbbed2782d0c31a44f9cb51a77e3
Dave Wadden <dwadden@cs.washington.edu>
2020-08-19 13:43:51 -0700

    Clean up debugging script.

--- scripts/debug/debug_forward_pass.py
+++ scripts/debug/debug_forward_pass.py
@@ -10,64 +10,71 @@ from dygie.data.iterators import batch_iterator
 from dygie.models import dygie
 
 
-parser = argparse.ArgumentParser(description="Debug forward pass of model.")
-parser.add_argument("training_config", type=str, help="Path to the config file.")
-parser.add_argument("--use_bert", action="store_true",
-                    help="If given, use the BERT model in the config. Else, use random embeddings.")
-parser.add_argument("--model-archive", type=str, default=None,
-                    help="If given, load an archived model instaed of initializing from scratch.")
-parser.add_argument("--max_instances")
+def get_args():
+    parser = argparse.ArgumentParser(description="Debug forward pass of model.")
+    parser.add_argument("training_config", type=str, help="Path to the config file.")
+    parser.add_argument("--use_bert", action="store_true",
+                        help="If given, use the BERT model in the config. Else, use random embeddings.")
+    parser.add_argument("--model-archive", type=str, default=None,
+                        help="If given, load an archived model instaed of initializing from scratch.")
+    parser.add_argument("--max_instances")
+
+    args = parser.parse_args()
+    return args
 
 
-args = parser.parse_args()
+def main():
+    args = get_args()
+    # Read config.
+    file_dict = json.loads(evaluate_file(args.training_config))
+    model_dict = file_dict["model"]
 
-# Read config.
-file_dict = json.loads(evaluate_file(args.training_config))
-model_dict = file_dict["model"]
+    if args.use_bert:
+        bert_name = model_dict["embedder"]["token_embedders"]["bert"]["model_name"]
+    else:
+        bert_name = None
 
-if args.use_bert:
-    bert_name = model_dict["embedder"]["token_embedders"]["bert"]["model_name"]
-else:
-    bert_name = None
+    # Hack to replace components that we're setting in the script.
+    for name in ["type", "embedder", "initializer", "module_initializer"]:
+        del model_dict[name]
 
-# Hack to replace components that we're setting in the script.
-for name in ["type", "embedder", "initializer", "module_initializer"]:
-    del model_dict[name]
+    # Create indexer.
+    if args.use_bert:
+        tok_indexers = {"bert": token_indexers.PretrainedTransformerMismatchedIndexer(
+            bert_name, max_length=512)}
+    else:
+        tok_indexers = {"tokens": token_indexers.SingleIdTokenIndexer()}
 
-# Create indexer.
-if args.use_bert:
-    token_indexers = {"bert": token_indexers.PretrainedTransformerMismatchedIndexer(
-        bert_name, max_length=512)}
-else:
-    token_indexers = {"tokens": token_indexers.SingleIdTokenIndexer()}
+    # Read input data.
+    reader = DyGIEReader(max_span_width=8, token_indexers=tok_indexers, max_instances=5)
+    data = reader.read(file_dict["train_data_path"])
+    vocab = vocabulary.Vocabulary.from_instances(data)
+    data.index_with(vocab)
 
-# Read input data.
-reader = DyGIEReader(max_span_width=8, token_indexers=token_indexers, max_instances=5)
-data = reader.read(file_dict["train_data_path"])
-vocab = vocabulary.Vocabulary.from_instances(data)
-data.index_with(vocab)
+    # Create embedder.
+    if args.use_bert:
+        token_embedder = token_embedders.PretrainedTransformerMismatchedEmbedder(
+            bert_name, max_length=512)
+        embedder = text_field_embedders.BasicTextFieldEmbedder({"bert": token_embedder})
+    else:
+        token_embedder = token_embedders.Embedding(
+            num_embeddings=vocab.get_vocab_size("tokens"), embedding_dim=100)
+        embedder = text_field_embedders.BasicTextFieldEmbedder({"tokens": token_embedder})
 
-# Create embedder.
-if args.use_bert:
-    token_embedder = token_embedders.PretrainedTransformerMismatchedEmbedder(
-        bert_name, max_length=512)
-    embedder = text_field_embedders.BasicTextFieldEmbedder({"bert": token_embedder})
-else:
-    token_embedder = token_embedders.Embedding(
-        num_embeddings=vocab.get_vocab_size("tokens"), embedding_dim=100)
-    embedder = text_field_embedders.BasicTextFieldEmbedder({"tokens": token_embedder})
+    # Create iterator and model.
+    iterator = batch_iterator.BatchIterator(batch_size=1, dataset=data)
+    if args.model_archive is None:
+        model = dygie.DyGIE(vocab=vocab,
+                            embedder=embedder,
+                            **model_dict)
+    else:
+        model = dygie.DyGIE.from_archive(args.model_archive)
 
-# Create iterator and model.
-iterator = batch_iterator.BatchIterator(batch_size=1, dataset=data)
-if args.model_archive is None:
-    model = dygie.DyGIE(vocab=vocab,
-                        embedder=embedder,
-                        **model_dict)
-else:
-    model = dygie.DyGIE.from_archive(args.model_archive)
+    # Run forward pass over a single entry.
+    for batch in iterator:
+        output_dict = model(**batch)
+        prediction = model.make_output_human_readable(output_dict)
 
 
-# Run forward pass over a single entry.
-for batch in iterator:
-    output_dict = model(**batch)
-    prediction = model.make_output_human_readable(output_dict)
+if __name__ == "__main__":
+    main()
