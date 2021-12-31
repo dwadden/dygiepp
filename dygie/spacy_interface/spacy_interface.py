6d170a1d3e7a29903629c6d37b340dfe7ce3c310
e3oroush <ebrahim.soroush@gmail.com>
2021-12-31 20:53:08 +0100

    Adds unit test and span based entities to spacy interface

--- dygie/spacy_interface/spacy_interface.py
+++ dygie/spacy_interface/spacy_interface.py
@@ -1,3 +1,4 @@
+from typing import Dict, List
 from allennlp.data import Batch
 from dygie.models.dygie import DyGIE
 from dygie.data.dataset_readers.dygie import DyGIEReader
@@ -10,8 +11,74 @@ from spacy.tokens.span import Span
 
 Doc.set_extension("rels", default=[], force=True)
 Span.set_extension("rels", default=[], force=True)
+Doc.set_extension("span_ents", default=[], force=True)
+Span.set_extension("label_", default=[], force=True)
 
 
+def prepare_spacy_doc(doc: Doc, prediction: Dict) -> Doc:
+    doc_rels = []
+    # store the relations to doc._.rels of spacy extension
+    for rels,ds in zip(prediction['predicted_relations'],doc.sents):
+        sent_rels = []
+        for rel in rels:
+            e1 = doc[rel[0]:rel[1]+1]
+            e2 = doc[rel[2]:rel[3]+1]
+            tag = rel[4]
+            sent_rels.append((e1,e2,tag))
+        doc_rels.append(sent_rels)
+        ds._.rels = sent_rels
+    doc._.rels = doc_rels
+
+    preds = [p for r in prediction['predicted_ner'] for p in r]
+    # storing all span based entitis to doc._.span_ents
+    span_ents = []
+    for sent in prediction['predicted_ner']:
+        ent_sent = []
+        for ent in sent:
+            d = doc[ent[0]:ent[1]+1]
+            d._.label_ = ent[2]
+            ent_sent.append(d)
+        span_ents.append(ent_sent)
+    doc._.span_ents = span_ents
+    # store entities to doc.ents of spacy
+    # because spacy can't support the overlapped entities we have to merge overlapped entities
+    # to the longest ones.
+    dist_ents = []
+    prc = []
+    for i,p1 in enumerate(preds):
+        t=[p1]
+        if i in prc:
+            continue
+        for j,p2 in enumerate(preds[i+1:]):
+            if p2[0]<=p1[1]:
+                t.append(p1)
+                prc.append(j+i+1)
+        dist_ents.append(t)
+    res = []
+    for t in dist_ents:
+        if len(t) == 1:
+            res.append(t[0])
+        elif len(t)>1:
+            mn = t[0][0]
+            mx = t[0][1]
+            for p in t[1:]:
+                if p[0]<mn:
+                    mn = p[0]
+                if p[1]>mx:
+                    mx=p[1]
+            res.append([mn,mx,t[0][2],t[0][3],t[0][4]])
+    sel_ents = []
+    for ent in res:
+        try:
+            d = doc[ent[0]:ent[1]+1]
+            s = doc.char_span(d.start_char, d.end_char, label=ent[2])
+            if s:
+                sel_ents.append(s)
+        except Exception as e:
+            print("error in spacy span", e)
+            raise e
+    doc.ents = sel_ents
+    return doc
 
 class DygieppPipe:
     name = 'dygiepp'
@@ -41,56 +108,6 @@ class DygieppPipe:
         dataset.index_instances(self._model.vocab)
         model_input = util.move_to_device(dataset.as_tensor_dict(), cuda_device)
         prediction = self._model.make_output_human_readable(self._model(**model_input)).to_json()
-        # store the relations to doc._.rels of spacy extension
-        doc_rels = []
-        for rels,ds in zip(prediction['predicted_relations'],doc.sents):
-            sent_rels = []
-            for rel in rels:
-                e1 = doc[rel[0]:rel[1]+1]
-                e2 = doc[rel[2]:rel[3]+1]
-                tag = rel[4]
-                sent_rels.append((e1,e2,tag))
-            doc_rels.append(sent_rels)
-            ds._.rels = sent_rels
-        doc._.rels = doc_rels
-        # store entities to doc.ents of spacy
-        preds = [p for r in prediction['predicted_ner'] for p in r]
-        # because spacy can't support the overlapped entities we have to merge overlapped entities
-        # to the longest ones.
-        dist_ents = []
-        prc = []
-        for i,p1 in enumerate(preds):
-            t=[p1]
-            if i in prc:
-                continue
-            for j,p2 in enumerate(preds[i+1:]):
-                if p2[0]<=p1[1]:
-                    t.append(p1)
-                    prc.append(j+i+1)
-            dist_ents.append(t)
-        res = []
-        for t in dist_ents:
-            if len(t) == 1:
-                res.append(t[0])
-            elif len(t)>1:
-                mn = t[0][0]
-                mx = t[0][1]
-                for p in t[1:]:
-                    if p[0]<mn:
-                        mn = p[0]
-                    if p[1]>mx:
-                        mx=p[1]
-                res.append([mn,mx,t[0][2],t[0][3],t[0][4]])
-        sel_ents = []
-        for ent in res:
-            try:
-                d = doc[ent[0]:ent[1]+1]
-                s = doc.char_span(d.start_char, d.end_char, label=ent[2])
-                if s:
-                    sel_ents.append(s)
-            except Exception as e:
-                print("error in spacy span", e)
-                raise e
-        doc.ents = sel_ents
-        return doc
+        # prepare and store ent/relation information to spacy Doc
+        return prepare_spacy_doc(doc, prediction)
 
